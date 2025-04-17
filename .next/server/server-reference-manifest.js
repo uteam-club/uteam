40ab8591072e5f4ea2bd9ca0c17a9a524ae7e3b7
Artem Oganisyan <oganisyan.coach@gmail.com>
2025-04-17 18:30:33 +0300

    Добавлен .gitignore для исключения node_modules и .next

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"uHwekHtG4XCXFBOr4SayJ/0391ZAqDoTMT7MybmnzHo=\"\n}"
--- node_modules/next-intl/plugin.d.ts
+++
@@ -1,6 +0,0 @@
-import {NextConfig} from 'next';
-
-function withNextIntl(i18nPath?: string): (config?: NextConfig) => NextConfig;
-
-// Currently only available via CJS
-export = withNextIntl;
