4d5da4923397aa1391afcb8d2e2589b9dc29e765
Artem Oganisyan <oganisyan.coach@gmail.com>
2025-07-25 15:55:32 +0300

    fix: use correct local auth-options for NextAuth in API route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import { getUserPermissions } from '@/services/user.service';
 import { hasPermission } from '@/lib/permissions';
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth-options";
+import { authOptions } from "./auth-options";
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
